b8273e65df365f05b238301fec15e8ecdc1e801c
Oleksandr Vinnyk <ovinnyk@deloitte.com>
2021-03-25 19:56:50 +0200

    added code to test GPIO

--- Piclimatic/Program.cs
+++ Piclimatic/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Device.Gpio;
+using System.Threading;
 
 namespace Piclimatic
 {
@@ -7,6 +9,15 @@ namespace Piclimatic
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            int pin = 37;
+            using var controller = new GpioController();
+            controller.OpenPin(pin, PinMode.Output);
+
+            controller.Write(pin, PinValue.High);
+            Thread.Sleep(1000);
+            controller.Write(pin, PinValue.Low);
+            Thread.Sleep(1000);
         }
     }
 }
